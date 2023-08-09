7e58cf6e69a66a7222009ecad6bdd7e1d5e64495
ZakMAryam <129435881+ZakMAryam@users.noreply.github.com>
2023-08-09 14:16:53 -0400

    first commit

---
+++ src/com/codemaryam/Main.java
@@ -0,0 +1,17 @@
+package com.codemaryam;
+
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	HashMap <String, String> vocabulary = new HashMap<>();
+    
+
+
+
+
+
+    }
+}
